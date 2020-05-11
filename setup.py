b594094bfe4cafa7e1b2eebbd6afdc47f59d4f26
Ismail Asega <i.asega@ug.isys.mobi>
2020-05-11 16:01:58 +0300

    ICTLife-Interview

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup #import setup function from the python setuptools Package
+from setuptools import setup #importing the setup function from the python setuptools Package
 setup(
-    name = 'ICTLife-CLI', #Project/Interview name
+    name = 'ICTLife-CLI', #CLI-Application name
     version = '1.0', #Application Version
     packages = ['ICTLifeInterview'], #Application Package
     entry_points = {
         'console_scripts': [
             'ICTLifeInterview = ICTLifeInterview.__main__:main'
-        ] #The application runnable is ICTLifeInterview, and when executed will 
+        ] #The application runnable is ICTLifeInterview, and when executed will
         #run the main function in the __main__ module which is part of the
         #ICTLifeInterview package
     })
