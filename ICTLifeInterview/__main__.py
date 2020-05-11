a91005c01a4a417dbaf99cbc0f85d2156f479700
Ismail Asega <i.asega@ug.isys.mobi>
2020-05-11 18:03:11 +0300

    ICTLife-Interview

--- ICTLifeInterview/__main__.py
+++ ICTLifeInterview/__main__.py
@@ -1,9 +1,12 @@
 import sys
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+
+    login=input('Please enter your Access ID: ')
+    for Access_ID in open('D:\ICTLife_credentials.txt', 'r').readlines():
+        Access_info = Access_ID.split()
+        if login == Access_info[0]:
+            print('correct credentials')
+
+
 if __name__ == '__main__':
     main()
