f0f25aa08bcb46915d241f0afcc825f3a28dfb7e
Ismail Asega <i.asega@ug.isys.mobi>
2020-05-11 15:44:33 +0300

    ICTLife-Interview

--- ICTLifeInterview/__main__.py
+++ ICTLifeInterview/__main__.py
@@ -1,14 +1,9 @@
 import sys
-#from .classmodule import ICTlife
-#from .funcmodule import ICTlife_function
 def main():
     print('in main')
     args = sys.argv[1:]
     print('count of args :: {}'.format(len(args)))
     for arg in args:
         print('passed argument :: {}'.format(arg))
-    #ICTlife_function('hello world')
-    #my_object = ICTlife('Thomas')
-    #my_object.say_name()
 if __name__ == '__main__':
     main()
