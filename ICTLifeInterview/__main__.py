76ca0119ec52f839dfe8ed50c309df73fd9e2306
Ismail Asega <i.asega@ug.isys.mobi>
2020-05-12 04:36:36 +0300

    ICTLife-Interview

--- ICTLifeInterview/__main__.py
+++ ICTLifeInterview/__main__.py
@@ -1,14 +1,24 @@
 import sys
-def main():
+def access_allowed(): #Validating IAM user using the assigned AWS_ACCESS_KEY_ID
+    AWS_ACCESS_KEY_ID = input('Please enter your AWS_ACCESS_KEY_ID: ')
+    for access_id in open('D:\ICTLife_credentials.txt', 'r'):
+            access_id = access_id.split()
+            if AWS_ACCESS_KEY_ID == access_id[2]:
+                return True
+    return False
 
-    AWS_ACCESS_KEY_ID=input('Please enter your AWS_ACCESS_KEY_ID: ')
-    for Access_ID in open('D:\ICTLife_credentials.txt', 'r').readlines():
-        Access_info = Access_ID.split()
-        if AWS_ACCESS_KEY_ID == Access_info[0]:
-            print('correct credentials')
-            return True
-        print("Incorrect credentials.")
-        return False
+if access_allowed():
+    print('\n' '******your IAM credentials******\n', open('D:\ICTLife_credentials.txt', 'r').read(-9), '\n' '******Keep them safe******', '\n')
+    print('Access Granted' '\n' 'press any key to continue')
+else:
+    print('Invalid IAM user')
+    sys.exit()
+
+def main():
+    args = sys.argv[1:]
+    print('count of args :: {}'.format(len(args)))
+    for arg in args:
+        print('passed argument :: {}'.format(arg))
 
 if __name__ == '__main__':
     main()
